44a6ba67241303dfd51c3a926f8d112eeed6e65e
Samarth Upadhyay <126957213+samarthupa@users.noreply.github.com>
2024-03-15 23:49:46 +0530

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -8,7 +8,17 @@ def check_status_and_redirection(url):
         status_code = response.status_code
         redirection_url = response.headers.get('location')
         if status_code in [301, 307, 302] and redirection_url:
-            return status_code, redirection_url
+            # If there are multiple redirections, recursively follow them
+            redirection_urls = [redirection_url]
+            while True:
+                response = requests.get(redirection_url, allow_redirects=False)
+                status_code = response.status_code
+                redirection_url = response.headers.get('location')
+                if status_code in [301, 307, 302] and redirection_url:
+                    redirection_urls.append(redirection_url)
+                else:
+                    break
+            return status_code, redirection_urls
         else:
             return status_code, "N/A"
     except Exception as e:
@@ -26,8 +36,8 @@ if st.button("Submit"):
     results = []
     for url in urls_list:
         headers = {"User-Agent": user_agents}
-        status_code, redirection_url = check_status_and_redirection(url)
-        results.append((url, status_code, redirection_url))
+        status_code, redirection_urls = check_status_and_redirection(url)
+        results.append((url, status_code, redirection_urls))
     
     # Display results in table
     st.table(results)
