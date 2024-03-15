5f7a8f5561f6c4bc9d814524f601b2e1789309a9
Samarth Upadhyay <126957213+samarthupa@users.noreply.github.com>
2024-03-15 23:55:48 +0530

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -1,10 +1,27 @@
 import streamlit as st
 import requests
 
+# Proxy servers for different countries
+proxies = {
+    'USA': 'http://us-proxy.org',
+    'UK': 'http://uk-proxy.org',
+    'Germany': 'http://de-proxy.org',
+    'Australia': 'http://au-proxy.org',
+    'Brazil': 'http://br-proxy.org',
+    'Japan': 'http://jp-proxy.org',
+    'South Africa': 'http://za-proxy.org',
+    'India': 'http://in-proxy.org',
+    'China': 'http://cn-proxy.org',
+}
+
 # Function to check status code and redirection of URL
-def check_status_and_redirection(url):
+def check_status_and_redirection(url, country=None):
     try:
-        response = requests.get(url, allow_redirects=False)
+        if country:
+            proxy = proxies.get(country)
+            response = requests.get(url, proxies={'http': proxy, 'https': proxy}, allow_redirects=False)
+        else:
+            response = requests.get(url, allow_redirects=False)
         status_code = response.status_code
         redirection_urls = []
         if status_code in [301, 307, 302]:
@@ -21,6 +38,7 @@ st.title("URL Analysis Tool")
 # Input fields
 urls = st.text_area("Enter URL(s) (one URL per line)", height=150)
 user_agents = st.selectbox("Choose User Agent", ["Chrome", "Firefox", "Safari"])
+country = st.selectbox("Choose Country", ["None", "USA", "UK", "Germany", "Australia", "Brazil", "Japan", "South Africa", "India", "China"])
 
 if st.button("Submit"):
     urls_list = urls.split('\n')
@@ -28,7 +46,7 @@ if st.button("Submit"):
     max_redirections = 0
     for url in urls_list:
         headers = {"User-Agent": user_agents}
-        status_code, redirection_urls = check_status_and_redirection(url)
+        status_code, redirection_urls = check_status_and_redirection(url, country)
         max_redirections = max(max_redirections, len(redirection_urls))
         results.append((url, status_code, *redirection_urls))
     
