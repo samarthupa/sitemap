cdad7ee41bb2fb36c418b991c69d36b1a664a7c1
Samarth Upadhyay <126957213+samarthupa@users.noreply.github.com>
2024-03-17 00:19:08 +0530

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -12,71 +12,85 @@ def check_status_and_redirection(url, user_agent):
         response = requests.get(url, headers=headers, allow_redirects=False)
         status_code = response.status_code
         redirection_urls = []
+        redirection_status_codes = []
         if status_code in [301, 307, 302]:
             while 'location' in response.headers:
-                redirection_urls.append(response.headers['location'])
-                response = requests.get(response.headers['location'], headers=headers, allow_redirects=False)
-        return status_code, redirection_urls
+                redirection_url = response.headers['location']
+                redirection_status_code = response.status_code
+                redirection_urls.append(redirection_url)
+                redirection_status_codes.append(redirection_status_code)
+                response = requests.get(redirection_url, headers=headers, allow_redirects=False)
+        return status_code, redirection_urls, redirection_status_codes
     except Exception as e:
-        return str(e), "N/A"
+        return str(e), "N/A", []
 
 # Streamlit UI
 st.title("URL Analysis Tool")
+st.sidebar.header("Options")
 
 # Input fields
 urls = st.text_area("Enter URL(s) (one URL per line)", height=150)
-user_agents = st.selectbox("Choose User Agent", ["Chrome", "Firefox", "Safari"])
+user_agents = st.sidebar.selectbox("Choose User Agent", ["Chrome", "Firefox", "Safari"])
+show_status_codes = st.sidebar.checkbox("Show Status Codes for Redirection URLs")
 
 if st.button("Submit"):
-    ua = UserAgent()
-    selected_user_agent = ""
-    if user_agents == "Chrome":
-        selected_user_agent = ua.chrome
-    elif user_agents == "Firefox":
-        selected_user_agent = ua.firefox
-    elif user_agents == "Safari":
-        selected_user_agent = ua.safari
+    with st.spinner("Analyzing URLs... It may take time if there are many URLs."):
+        ua = UserAgent()
+        selected_user_agent = ""
+        if user_agents == "Chrome":
+            selected_user_agent = ua.chrome
+        elif user_agents == "Firefox":
+            selected_user_agent = ua.firefox
+        elif user_agents == "Safari":
+            selected_user_agent = ua.safari
 
-    urls_list = urls.split('\n')
-    results = []
-    max_redirections = 0
-    final_destinations = []
-    for url in urls_list:
-        status_code, redirection_urls = check_status_and_redirection(url, selected_user_agent)
-        max_redirections = max(max_redirections, len(redirection_urls))
-        results.append((url, status_code, *redirection_urls))
-        final_destination = redirection_urls[-1] if redirection_urls else url
-        final_destinations.append((url, final_destination))
-    
-    # Prepare column headers for main sheet
-    main_headers = ['URL', 'Status Code']
-    for i in range(max_redirections):
-        main_headers.append(f'Redirection URL {i+1}')
+        urls_list = urls.split('\n')
+        results = []
+        max_redirections = 0
+        final_destinations = []
+        for url in urls_list:
+            status_code, redirection_urls, redirection_status_codes = check_status_and_redirection(url, selected_user_agent)
+            max_redirections = max(max_redirections, len(redirection_urls))
+            if show_status_codes:
+                results.append((url, status_code, *zip(redirection_urls, redirection_status_codes)))
+            else:
+                results.append((url, status_code, *redirection_urls))
+            final_destination = redirection_urls[-1] if redirection_urls else url
+            final_destinations.append((url, final_destination))
 
-    # Prepare data for main sheet
-    main_data = [main_headers] + results
+        # Prepare column headers for main sheet
+        main_headers = ['URL', 'Status Code']
+        if show_status_codes:
+            for i in range(max_redirections):
+                main_headers.extend([f'Redirection URL {i+1}', f'Redirection Status Code {i+1}'])
+        else:
+            for i in range(max_redirections):
+                main_headers.append(f'Redirection URL {i+1}')
 
-    # Prepare data for fix redirection sheet
-    fix_redirection_headers = ['Original URL', 'Final Destination URL']
-    fix_redirection_data = [fix_redirection_headers] + final_destinations
+        # Prepare data for main sheet
+        main_data = [main_headers] + results
 
-    # Create Excel file with two sheets
-    excel_data = {'Main Sheet': main_data, 'Fix Redirection': fix_redirection_data}
-    excel_file = BytesIO()
-    excel_writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
-    for sheet_name, sheet_data in excel_data.items():
-        df = pd.DataFrame(sheet_data)
-        df.to_excel(excel_writer, index=False, sheet_name=sheet_name, header=False)
-    excel_writer.close()  # Close the writer
-    excel_file.seek(0)
+        # Prepare data for fix redirection sheet
+        fix_redirection_headers = ['Original URL', 'Final Destination URL']
+        fix_redirection_data = [fix_redirection_headers] + final_destinations
 
-    # Display results in table
-    st.table(main_data)
+        # Create Excel file with two sheets
+        excel_data = {'Main Sheet': main_data, 'Fix Redirection': fix_redirection_data}
+        excel_file = BytesIO()
+        excel_writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+        for sheet_name, sheet_data in excel_data.items():
+            df = pd.DataFrame(sheet_data)
+            df.to_excel(excel_writer, index=False, sheet_name=sheet_name, header=False)
+        excel_writer.close()  # Close the writer
+        excel_file.seek(0)
 
-    # Download button for Excel file
-    st.download_button(
-        label="Download Excel",
-        data=excel_file,
-        file_name="url_analysis_results.xlsx",
-        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )
+        # Display results in table
+        st.table(main_data)
+
+        # Download button for Excel file
+        st.download_button(
+            label="Download Excel",
+            data=excel_file,
+            file_name="url_analysis_results.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
