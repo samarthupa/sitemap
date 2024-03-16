4d82234b22051d31cb03ce5d8af400e5450177b9
Samarth Upadhyay <126957213+samarthupa@users.noreply.github.com>
2024-03-17 00:27:12 +0530

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -16,13 +16,12 @@ def check_status_and_redirection(url, user_agent):
         if status_code in [301, 307, 302]:
             while 'location' in response.headers:
                 redirection_url = response.headers['location']
-                redirection_status_code = requests.get(redirection_url, headers=headers, allow_redirects=False).status_code
                 redirection_urls.append(redirection_url)
-                redirection_status_codes.append(redirection_status_code)
+                redirection_status_codes.append(response.status_code)
                 response = requests.get(redirection_url, headers=headers, allow_redirects=False)
         return status_code, redirection_urls, redirection_status_codes
     except Exception as e:
-        return str(e), "N/A", "N/A"
+        return str(e), "N/A", []
 
 # Streamlit UI
 st.title("URL Analysis Tool")
@@ -32,7 +31,8 @@ urls = st.text_area("Enter URL(s) (one URL per line)", height=150)
 user_agents = st.selectbox("Choose User Agent", ["Chrome", "Firefox", "Safari"])
 
 if st.button("Submit"):
-    with st.spinner("Analyzing URLs... It may take time if there are many URLs"):
+    st.text("Analyzing URLs. It may take time if there are many URLs...")
+    with st.spinner():
         ua = UserAgent()
         selected_user_agent = ""
         if user_agents == "Chrome":
@@ -45,13 +45,10 @@ if st.button("Submit"):
         urls_list = urls.split('\n')
         results = []
         max_redirections = 0
-        final_destinations = []
         for url in urls_list:
             status_code, redirection_urls, redirection_status_codes = check_status_and_redirection(url, selected_user_agent)
             max_redirections = max(max_redirections, len(redirection_urls))
             results.append((url, status_code, *redirection_urls, *redirection_status_codes))
-            final_destination = redirection_urls[-1] if redirection_urls else url
-            final_destinations.append((url, final_destination))
         
         # Prepare column headers for main sheet
         main_headers = ['URL', 'Status Code']
@@ -61,12 +58,8 @@ if st.button("Submit"):
         # Prepare data for main sheet
         main_data = [main_headers] + results
 
-        # Prepare data for fix redirection sheet
-        fix_redirection_headers = ['Original URL', 'Final Destination URL']
-        fix_redirection_data = [fix_redirection_headers] + final_destinations
-
-        # Create Excel file with two sheets
-        excel_data = {'Main Sheet': main_data, 'Fix Redirection': fix_redirection_data}
+        # Create Excel file with one sheet
+        excel_data = {'Main Sheet': main_data}
         excel_file = BytesIO()
         excel_writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
         for sheet_name, sheet_data in excel_data.items():
